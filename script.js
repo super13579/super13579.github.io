c41a3a3c2258b34e62054f834700dca039fae0b1
super13579 <super13579@muenai.com>
2021-03-17 12:27:53 +0800

    Update web page

---
+++ script.js
@@ -0,0 +1,99 @@
+var map = L.map('map', {
+    center: [22.604964, 120.300476],
+    zoom: 16
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([25.0585312, 121.6061037]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10271734.html">最優質企業總部交通便捷提升企業形象首選</a><h1>150,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0554198, 121.6131876]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10302709.html">南港軟體園區~辦公室~近捷運交通便利</a><h1>18,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0508308, 121.5943680]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10273041.html">★賽鉑倫商務辦公室個人桌7千有找★</a><h1>6,500 元/月</h1>')
+    .openPopup();
+L.marker([25.0499564, 121.5795814]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10352689.html">松山車站旁辦公室營業地址出租</a><h1>2,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0466213, 121.5836722]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10526536.html">頂級純辦公氣派大方後山埤站稀有把握機會!</a><h1>110,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0572540, 121.6018180]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10620865.html">氣派大樓、採光明亮、格局方正好規劃</a><h1>254,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0560551, 121.5898749]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10579656.html">河岸景觀,門廳氣派,彩光棒.視野好</a><h1>100,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0516995, 121.5948494]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10622187.html">💲簡約裝潢即進駐近捷運💲專車接送看屋</a><h1>372,400 元/月</h1>')
+    .openPopup();
+L.marker([25.0588380, 121.6047580]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10542942.html">企業東移南港科技辦方正簡約明亮環境優!</a><h1>152,381 元/月</h1>')
+    .openPopup();
+L.marker([25.056135, 121.59055]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10454096.html">昆陽捷運站~面河景一層ㄧ戶~屋優~</a><h1>160,000 元/月 租金波動</h1>')
+    .openPopup();
+L.marker([25.0551892, 121.5892360]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10621225.html">昆陽捷運站~視野好屋優~立即可用~</a><h1>100,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0501105, 121.5932604]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10625467.html">昆陽站尊榮辦視野唯美環境優現成裝潢超值選</a><h1>354,667 元/月</h1>')
+    .openPopup();
+L.marker([25.0551690, 121.6151870]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10620359.html">南港展覽館站~挑高方正~屋況佳~</a><h1>100,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0553424, 121.5913981]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10314813.html">南港科技辦租金實惠時尚舒適辦公環境優!</a><h1>197,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0537071, 121.5917962]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10582290.html">辦公室權狀196坪出租方正格局</a><h1>176,400 元/月 租金波動</h1>')
+    .openPopup();
+L.marker([25.0554960, 121.5928350]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10413267.html">戰勝不景氣!近昆陽站南港科技辦節流首選.</a><h1>295,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0588380, 121.6047580]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10279056.html">入主亞太雲端城市贏在起跑點台北第二世貿區</a><h1>432,276 元/月</h1>')
+    .openPopup();
+L.marker([25.0588747, 121.6137051]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10380624.html">南軟園區全新氣派樓近捷運火車站東移新選擇</a><h1>234,285 元/月</h1>')
+    .openPopup();
+L.marker([25.0467642, 121.5826678]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10411989.html">北辦東移媲美信義商圈頂級辦門廳氣派機能讚</a><h1>110,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0574659, 121.5914480]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10612033.html">廠辦大樓出租權狀212.08坪稀有物件</a><h1>196,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0531390, 121.5912440]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10612065.html">園區商圈漂亮辦公方正氣派大樓門廳</a><h1>197,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0552079, 121.5932725]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10383529.html">捷運高級商辦大樓約106坪稀有物件</a><h1>98,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0499523, 121.5777326]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10577232.html">四鐵共構松車文清藝術風時尚辦商圈機能優</a><h1>132,420 元/月</h1>')
+    .openPopup();
+L.marker([25.0551690, 121.6151870]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10611984.html">近捷運辦公大樓，南港軟體園區附近，可分租</a><h1>100,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0501105, 121.5932604]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10616770.html">昆陽捷運辦公室</a><h1>372,400 元/月</h1>')
+    .openPopup();
+L.marker([25.0568471, 121.6008550]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10454862.html">小房東*南港重陽*大空間商辦*誠可議</a><h1>145,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0466451, 121.5836960]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10257586.html">雙捷運景觀戶~A辦氣派門廳~附玻璃隔間</a><h1>118,100 元/月</h1>')
+    .openPopup();
+L.marker([25.0525582, 121.604518]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10285196.html">⭐免仲介費⭐南港車站*三鐵共構*氣派大樓</a><h1>3,492,525 元/月</h1>')
+    .openPopup();
+L.marker([25.0547209, 121.5907766]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10509058.html">園區精美辦公室87519898低公設廠辦</a><h1>196,000 元/月</h1>')
+    .openPopup();
+L.marker([25.0499523, 121.5777326]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10278064.html">◆宜家國際地產◆低公設/使用性佳/松山站</a><h1>139,041 元/月</h1>')
+    .openPopup();
