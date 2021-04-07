9e04a3312777a63bef3d13ca6adb5db764836184
super13579 <robert200232003@hotmail.com>
2021-04-07 12:27:37 +0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var map = L.map('map', {
-    center: [25.0297577, 121.4838459],
+    center: [24.9988856, 121.4593237],
     zoom: 20
 });
 
@@ -8,7 +8,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+var myIcon = L.icon({
+    iconUrl: 'Toicon-icon-avocado-locate.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -40]
 
+});
 
 L.marker([25.0067894, 121.4616680]).addTo(map)
     .bindPopup('<a href="https://rent.591.com.tw/rent-detail-9899400.html">個人/共享辦公室輔導個人低成本創業</a><h1>3,500 元/月</h1><h1>坪數 :  30坪</h1>');
@@ -54,12 +60,101 @@ L.marker([25.0278228, 121.4820878]).addTo(map)
 L.marker([25.0297577, 121.4838459]).addTo(map)
     .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10354446.html">★超大清幽公寓2F★</a><h1>22,000 元/月</h1><h1>坪數 :  46.48坪</h1>');
 
-var myIcon = L.icon({
-    iconUrl: 'Toicon-icon-avocado-locate.png',
-    iconSize: [32, 32],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -40]
 
-});
+L.marker([25.0206764, 121.4817697]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10578256.html">近光復橋~店面清淨~辦公適合</a><h1>35,000 元/月</h1><h1>坪數 :  35坪</h1>');
+L.marker([24.9993048, 121.4485423]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10597555.html">亞東捷運黃金店舖</a><h1>35,000 元/月</h1><h1>坪數 :  26坪</h1>');
+L.marker([24.9984846, 121.4425022]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10543453.html">1樓店面出租</a><h1>25,000 元/月</h1><h1>坪數 :  30坪</h1>');
+L.marker([24.9984831, 121.4310534]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10466742.html">超級城市店面出租</a><h1>29,500 元/月</h1><h1>坪數 :  27坪</h1>');
+L.marker([25.0203089, 121.4754050]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10735057.html">板橋店面住辦門口可停車</a><h1>33,000 元/月</h1><h1>坪數 :  27坪</h1>');
+L.marker([24.9912315, 121.4318848]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10732955.html">學區綠地旁緊鄰篤行路的方正深長型店面</a><h1>32,000 元/月</h1><h1>坪數 :  40坪</h1>');
+L.marker([25.0302017, 121.4781859]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10680175.html">文聖街店面辦公室</a><h1>33,000 元/月</h1><h1>坪數 :  40坪</h1>');
+L.marker([25.0162824, 121.4759144]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10512926.html">中山人潮2F店面</a><h1>26,000 元/月</h1><h1>坪數 :  22.36坪</h1>');
+L.marker([25.0162824, 121.4759144]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10681240.html">埔墘市場店面2F副店面</a><h1>26,000 元/月</h1><h1>坪數 :  27坪</h1>');
+L.marker([25.0164114, 121.4783523]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10722095.html">適合辦公室、事務所、維修便宜稀有物件</a><h1>28,000 元/月</h1><h1>坪數 :  25坪</h1>');
+L.marker([25.0070736, 121.4569295]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10369723.html">府中捷運(近南雅夜市)巷內店辦</a><h1>27,000 元/月</h1><h1>坪數 :  27坪</h1>');
+L.marker([25.0199530, 121.4625850]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-6597234.html">近板橋車站致理商專停車場旁商圈漂亮小店面</a><h1>30,000 元/月</h1><h1>坪數 :  25坪</h1>');
+L.marker([25.0302540, 121.4754440]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10223261.html">江子翠捷運松柏街二樓店辦</a><h1>25,000 元/月</h1><h1>坪數 :  23坪</h1>');
+L.marker([24.9913092, 121.4310872]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10657265.html">存德金店面、可登記、租金含管</a><h1>35,000 元/月</h1><h1>坪數 :  39.98坪</h1>');
+L.marker([25.0293473, 121.4719907]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10564975.html">江翠捷運口住辦(適美容.補教.辦公.教學</a><h1>33,000 元/月</h1><h1>坪數 :  32.09坪</h1>');
+L.marker([24.9992246, 121.4301762]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10405760.html">super讚超級城市漂亮店面價錢可談</a><h1>35,000 元/月</h1><h1>坪數 :  22.176坪</h1>');
+L.marker([24.9936425, 121.4499421]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10561029.html">四川路車水馬龍賺錢黃金店面</a><h1>35,000 元/月</h1><h1>坪數 :  32坪</h1>');
+L.marker([25.0172281, 121.4699688]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10703488.html">豪宅阿亮~縣民大道店辦</a><h1>35,000 元/月</h1><h1>坪數 :  28坪</h1>');
+L.marker([25.0172281, 121.4699688]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10679370.html">豪宅阿亮~縣民大道店辦</a><h1>35,000 元/月</h1><h1>坪數 :  28坪</h1>');
+L.marker([24.9900725, 121.4293346]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10555716.html">溪洲門口停車倉庫住辦</a><h1>30,000 元/月</h1><h1>坪數 :  40坪</h1>');
+L.marker([25.0269369, 121.4607870]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10568946.html">四維路1樓大空間（門口可停車）已收訂</a><h1>26,000 元/月</h1><h1>坪數 :  30坪</h1>');
+L.marker([25.0172281, 121.4699688]).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10602961.html">豪宅阿亮~縣民大道店辦</a><h1>35,000 元/月</h1><h1>坪數 :  28坪</h1>');
+
+L.marker([25.0119713, 121.4742517],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10620945.html">海山學區1樓全套傢俱家電配備齊全</a><h1>25,500 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  1F/4F</h1>');
+L.marker([25.0119713, 121.4742517],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10620945.html">海山學區1樓全套傢俱家電配備齊全</a><h1>25,500 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  1F/4F</h1>');
+L.marker([25.0167611, 121.4771935],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10630595.html">埔墘公園一樓新裝潢附全新冷氣2房2廳1衛</a><h1>25,000 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  1F/3F</h1>');
+L.marker([25.0166090, 121.4770400],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10602531.html">埔墘全新裝潢2房一廳一衛一廚</a><h1>23,000 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  1F/3F</h1>');
+L.marker([25.0260854, 121.4569414],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10610300.html">【板橋國中、玫瑰公園】→低樓層華廈三房</a><h1>22,500 元/月</h1><h1>坪數 :  35.82坪</h1><h1>樓層 :  2F/6F</h1>');
+L.marker([24.9889195, 121.4251545],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10733490.html">板橋溪洲公園旁2樓4房公寓免爬高(自租)</a><h1>24,200 元/月</h1><h1>坪數 :  39.8坪</h1><h1>樓層 :  2F/5F</h1>');
+L.marker([25.0073375, 121.4712025],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10729579.html">新板特區🍈海山學區優質公寓2樓</a><h1>22,000 元/月</h1><h1>坪數 :  24坪</h1><h1>樓層 :  2F/4F</h1>');
+L.marker([25.0167611, 121.4771935],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10630595.html">埔墘公園一樓新裝潢附全新冷氣2房2廳1衛</a><h1>25,000 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  1F/3F</h1>');
+L.marker([25.0166090, 121.4770400],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10602531.html">埔墘全新裝潢2房一廳一衛一廚</a><h1>23,000 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  1F/3F</h1>');
+L.marker([25.0260854, 121.4569414],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10610300.html">【板橋國中、玫瑰公園】→低樓層華廈三房</a><h1>22,500 元/月</h1><h1>坪數 :  35.82坪</h1><h1>樓層 :  2F/6F</h1>');
+L.marker([24.9889195, 121.4251545],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10733490.html">板橋溪洲公園旁2樓4房公寓免爬高(自租)</a><h1>24,200 元/月</h1><h1>坪數 :  39.8坪</h1><h1>樓層 :  2F/5F</h1>');
+L.marker([25.0073375, 121.4712025],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10729579.html">新板特區🍈海山學區優質公寓2樓</a><h1>22,000 元/月</h1><h1>坪數 :  24坪</h1><h1>樓層 :  2F/4F</h1>');
+L.marker([24.993934, 121.464396],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10658760.html">五權街復古式公寓,CP值高</a><h1>18,000 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  2F/5F</h1>');
+L.marker([25.0335963, 121.4785013],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10665397.html">使用約38坪!江子翠站7分鐘!住辦2樓</a><h1>21,000 元/月</h1><h1>坪數 :  38坪</h1><h1>樓層 :  2F/4F</h1>');
+L.marker([25.0085770, 121.4554994],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10630446.html">近南雅夜市2+3+4樓出租,附部份傢俱</a><h1>25,000 元/月</h1><h1>坪數 :  36.44坪</h1><h1>樓層 :  2F/2F</h1>');
+L.marker([25.0056056, 121.4558971],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10686523.html">《社會住宅》南雅靜巷一樓美屋</a><h1>25,000 元/月</h1><h1>坪數 :  30.59坪</h1><h1>樓層 :  1F/5F</h1>');
+L.marker([25.0068958, 121.4452431],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10718866.html">僑中二街浮州車站三房兩廳</a><h1>25,000 元/月</h1><h1>坪數 :  30坪</h1><h1>樓層 :  1F/5F</h1>');
+L.marker([25.0031821, 121.4429086],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10696970.html">一樓住店，門口停車</a><h1>25,000 元/月</h1><h1>坪數 :  33坪</h1><h1>樓層 :  1F/4F</h1>');
+L.marker([25.0173831, 121.4605106],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10691814.html">板橋區整層出租,適合小家庭近板橋捷運站</a><h1>26,000 元/月</h1><h1>坪數 :  25坪</h1><h1>樓層 :  1F/5F</h1>');
+L.marker([25.0137770, 121.4474270],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10718851.html">僑中一街舒適溫馨二房稀有1樓出租屋況佳</a><h1>20,500 元/月</h1><h1>坪數 :  23坪</h1><h1>樓層 :  1F/5F</h1>');
+L.marker([25.0096612, 121.4448602],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10690020.html">小資族僑中美寓一樓公寓免爬高樓近學區</a><h1>18,000 元/月</h1><h1>坪數 :  22坪</h1><h1>樓層 :  1F/5F</h1>');
+L.marker([25.0153799, 121.4710560],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10636999.html">板新路《適合辦公/住辦》二樓免爬高！</a><h1>27,000 元/月</h1><h1>坪數 :  24.6坪</h1><h1>樓層 :  2F/5F</h1>');
+L.marker([24.9988856, 121.4593237],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10731018.html">重慶國中*亞東醫院【標準3房2廳】免爬梯</a><h1>29,000 元/月</h1><h1>坪數 :  32坪</h1><h1>樓層 :  1F/5F</h1>');
+L.marker([25.0318002, 121.4655201],{icon: myIcon}).addTo(map)
+    .bindPopup('<a href="https://rent.591.com.tw/rent-detail-10725915.html">板橋長江路公寓～超值！</a><h1>18,000 元/月</h1><h1>坪數 :  27.25坪</h1><h1>樓層 :  2F/5F</h1>');
+
+
 
 L.control.locate().addTo(map);
